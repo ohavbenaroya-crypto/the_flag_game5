ef94c5c2eb7e9059e913ef7d13a7baaf40bbde7f
ohav <ohavbenaroya@gmail.com>
2026-08-18 10:02:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,68 @@
+from operator import index
+
 import pygame
+import time
+
+import consts
 from consts import *
 from screen import Screen
 from soldier import Soldier
 from game_field import GameField
-import time
+
+# def Buttons(key_pres):   #פונקציה שמחשבת את המיקום של אותו מיקום של מספר
+#  input = ''
+#  cont = 0
+#  # for event in pygame.event.get():
+#  #    if event.type == pygame.QUIT:
+#  #         running = False
+#
+#  for i in buttons:
+#         if i == key_pres:
+#             break
+#         else: cont+= 1
+#  return cont
+
+
+
+# def conting_butuns_time(event):
+#     start_time = None
+#     press_duration = 0
+#
+#     if event == pygame.KEYDOWN and event.key == pygame.K_1:
+#         start_time = pygame.time.get_ticks()
+#
+#     if event == pygame.KEYUP and event.key == pygame.K_1:
+#             press_duration = pygame.time.get_ticks() - start_time
+#
+#     return press_duration
+#
+
+
+
+
+# def cont_time_buttons():
+#     pygame.init()
+#     clock = pygame.time.Clock()
+#
+#     start_time = None
+#     press_duration = 0
+#
+#     # running = True
+#     # while running:
+#     for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 running = False
+#
+#             if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+#               start_time = pygame.time.get_ticks()
+#
+#             if event.type == pygame.KEYUP and event.key == pygame.K_1:
+#                 press_duration = pygame.time.get_ticks() - start_time
+#                 time_cont=press_duration/1000
+#
+#         # clock.tick(60)
+
+
 
 
 def main():
@@ -12,41 +71,52 @@ def main():
     soldier = Soldier()
     clock = pygame.time.Clock()
 
+
+
     show_mines = False
     mines_timer = 0
     is_game_over = False
     message = ""
     message_color = WHITE
-    is_running = True
+    running = True
+    
+    the_key = 0
+    # זיהוי של הממספר שהשחקן רוצה ללחוץ
 
-    while is_running:
+    while running:
         current_time = pygame.time.get_ticks()
 
         if show_mines:
-            if current_time - mines_timer >= 1000:
-                show_mines = False
+                if current_time - mines_timer >= 1000:
+                  show_mines = False
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                is_running = False
+                      if event.type == pygame.QUIT:
+                          is_running = False
+                      if event.type == pygame.KEYDOWN:
+                          if is_game_over == False:
+                              if show_mines == False:
+                                  if event.key == pygame.K_UP:
+                                      soldier.move(-1, 0)
+                                  elif event.key == pygame.K_DOWN:
+                                      soldier.move(1, 0)
+                                  elif event.key == pygame.K_LEFT:
+                                      soldier.move(0, -1)
+                                  elif event.key == pygame.K_RIGHT:
+                                      soldier.move(0, 1)
 
-            if event.type == pygame.KEYDOWN:
-                if is_game_over == False:
-                    if show_mines == False:
-                        if event.key == pygame.K_UP:
-                            soldier.move(-1, 0)
-                        elif event.key == pygame.K_DOWN:
-                            soldier.move(1, 0)
-                        elif event.key == pygame.K_LEFT:
-                            soldier.move(0, -1)
-                        elif event.key == pygame.K_RIGHT:
-                            soldier.move(0, 1)
+                              if event.key == pygame.K_RETURN:
+                                  show_mines = True
+                                  mines_timer = current_time
 
-                    if event.key == pygame.K_RETURN:
 
-                        show_mines = True
-                        mines_timer = current_time
 
+                              if event.key == pygame.K_0 - the_key:
+                                t = time.time()
+                      if event.type == pygame.KEYUP:
+                          if event.key == pygame.K_0 - the_key:  # key 'a'
+                              t = time.time() - t;t = str(t); t = t[:5]
+                              print("You pressed key 'a' for", t, 'seconds')
 
 
         if is_game_over == False:
@@ -63,19 +133,18 @@ def main():
                 message_color = WHITE
 
         screen.clear()
-        #
+
         if show_mines:
             screen.draw_grid()
             screen.draw_landmines(field.landmines)
             screen.draw_soldier_nigth(soldier.row, soldier.col)
-# הגדרתי את החייל לילה בכונסטאס ובסקרין והוספתי פעולה של ציור חייל לילה ופשוט מחקתי את הציור חייל שהיה לא בתוך לולאה אל האלס ואת ציור חייל לילה ב if
         else:
-            screen.draw_flag(field.flag_row, field.flag_col)
             screen.draw_bushes(field.bushes)
+            screen.draw_flag(field.flag_row, field.flag_col)
             screen.draw_soldier(soldier.row, soldier.col)
-# מחקתי את הציור דגל מחוץ ללולאה אל הלולאה של ה else
+            screen.draw_welcome_message()
 
-        screen.draw_welcome_message()
+
 
         if is_game_over:
             screen.draw_message(message, message_color)
